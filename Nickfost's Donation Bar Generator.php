36e5d5c081976cdebeb8196446cae3f2a286d558
Nick Foster <Me@nickfost.com>
2012-08-08 18:49:09 -0500

    Fixed the rounding of the percent  (fixes #1)

--- Nickfost's Donation Bar Generator.php
+++ Nickfost's Donation Bar Generator.php
@@ -22,7 +22,7 @@
  $redwidth = $width - $greenwidth;
  //percent in red caclulations
  if ($percentinred == on){
-	 $percentinred = round($decimalplacelimit,$percentinred);
+	 $percent = round($decimalplacelimit,$percent);
 	 if ($percent < 95) {
 		 $enabledpercentinred = $percent. "%";
 	 } 
