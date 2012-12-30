d2d51806cd699b1c8b7ca83dd7f7de4dce6c95a6
PigBacon <chris@gibsonsec.org>
2012-12-31 00:32:08 +1100

    HTML complete, script cleaned more, README now in markdown.

--- donation_bar.php
+++ donation_bar.php
@@ -7,7 +7,7 @@
 	// Total amount you're looking for. (default: 100)
 	$bar_conf['cost'] = 100;
 	// Current amount donated. (TODO: Fetch from somewhere like PayPal?) (default: 0)
-	$bar_conf['donations'] = 67;
+	$bar_conf['donations'] = 0;
 	
 	// Width of the bar;  can be 100%, 100px, etc. (default: 100%)
 	$bar_conf['width'] = '100%';
@@ -16,7 +16,7 @@
 	// Obviously the size of the font in em/px. (default: 10px)
 	$bar_conf['font_size'] = '10px';
 	
-	// Should we show how much is fullfilled, how much is needed, etc? (default: false)
+	// Should we show how much is fullfilled, how much is needed, etc? (default: false) (TODO! Not functional)
 	$bar_conf['show_stats'] = false;
 	// CSS color for completed amount. (default: #68a976)
 	$bar_conf['color_complete'] = '#68a976';
@@ -29,7 +29,7 @@
 	$bar_conf['decimal_places'] = 2;
 
 	// This will just dump information about the configuration. (default: false)
-	$bar_conf['debug'] = true;
+	$bar_conf['debug'] = false;
 
 	//---------------
 	// Math
@@ -38,12 +38,38 @@
 
 	$math['ratio'] = ($bar_conf['donations']) / $bar_conf['cost'];
 	$math['percent'] = ($math['ratio'] != 0 ? $math['ratio'] * 100 : 100);
-	$math['p_green'] = ($math['ratio'] * 100) . '%';
+	$math['p_green'] = (($math['ratio'] * 100) - 1) . '%';
 	$math['p_red'] = 100 - ($math['ratio'] * 100) . '%';
 ?>
 <style>
+div#donations-bar {
+	width: 100%;
+}
+div#donations-bar-inner {
+	width: <?php echo $bar_conf['width'] ?>;
+	height: <?php echo $bar_conf['height'] ?>;
+	font-size: 0;
+}
+div.donations-bar-inner {
+	height: 100%;
+	display: inline-block;
+	margin: 0 auto;
+	padding: 0;
+}
+div.donations-bar-inner.green {
+	background-color: <?php echo $bar_conf['color_complete'] ?>;
+	width: <?php echo $math['p_green'] ?>;
+}
+div.donations-bar-inner.red {
+	background-color: <?php echo $bar_conf['color_missing'] ?>;
+	width: <?php echo $math['p_red'] ?>;
+}
 </style>
 <div id="donations-bar">
+	<div id="donations-bar-inner">
+		<div class="donations-bar-inner green"></div>
+		<div class="donations-bar-inner red"></div>
+	</div>
 	<!-- Want your own bar? https://github.com/Nickfost/Donations-Bar-Generator -->
 </div>
 <?php
@@ -58,6 +84,5 @@ if ($bar_conf['debug']) {
 	echo var_dump($math, true);
 	echo '</pre> ' . PHP_EOL;
 	echo '</p>';
-	exit;
 }
 ?>
